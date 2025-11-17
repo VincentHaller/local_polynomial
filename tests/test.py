d1c4d15720f6c5acd9f8dee6fdfd98d35010fce0
VincentHaller <vincenthaller2@gmail.com>
2025-11-17 22:52:00 +0100

    a little bit of strange behavior in the symmetric loess

--- tests/test.py
+++ tests/test.py
@@ -6,13 +6,28 @@ from local_polynomial.local_polynomial import local_polynomial
 from local_polynomial.loess import loess
 
 # %%
-X = np.linspace(0, 4*np.pi, 200)
+X = np.linspace(-0.5, 4.5*np.pi, 200)
 Y = np.sin(X)
 
 # %%
 y_out =  local_polynomial(x_in = X, y_in = Y, x_out = X, q=0.3, degree=1)
-y_out_2 = loess(y_in=Y,q=3, asymmetric=0)
+y_out_2 = loess(y_in=Y,q=15, asymmetric=1)
 plt.scatter(X, Y)
 plt.scatter(X, y_out_2)
+
+# %%
+y_out_2
+
+# %%
+Y
+
+
+# %%
+y_out.shape
+
+# %%
+X.shape
+# %%
+y_out_2.shape
 
 # %%
