4b85df43a6bbb25ac63a5adb411c158ec1029961
VincentHaller <vincenthaller2@gmail.com>
2025-11-17 22:46:54 +0100

    making sure it can be run

--- tests/test.py
+++ tests/test.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from local_polynomial.local_polynomial import local_polynomial
+from local_polynomial.loess import loess
 
 # %%
 X = np.linspace(0, 4*np.pi, 200)
@@ -10,7 +11,8 @@ Y = np.sin(X)
 
 # %%
 y_out =  local_polynomial(x_in = X, y_in = Y, x_out = X, q=0.3, degree=1)
+y_out_2 = loess(y_in=Y,q=3, asymmetric=0)
 plt.scatter(X, Y)
-plt.scatter(X, y_out)
+plt.scatter(X, y_out_2)
 
 # %%
